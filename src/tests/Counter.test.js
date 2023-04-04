b8773c5591ae45ba9fa45d73b36ee20b2612bdd3
gonzalexa <alexa.95746@gmail.com>
2023-04-04 13:03:53 -0400

    Automated Unit Testing Assignment AG

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,38 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import React, { useState } from 'react';
+import { render, screen , fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('0');
+  
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId('count');
+  const incrementButton = screen.getByText('+');
+  fireEvent.click(incrementButton);
+
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId('count');
+  const decrementButton = screen.getByText('-');
+  fireEvent.click(decrementButton);
 });
